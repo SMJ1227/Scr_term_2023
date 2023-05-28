5def4c2331914ae0d6cd10991d10ff2411099e4f
sjm@MainPC <joy001227@gmail.com>
2023-05-29 03:00:49 +0900

    노트북1 완성

--- term.py
+++ term.py
@@ -80,9 +80,9 @@ for item in data:
     exchange_rate = item['deal_bas_r'] # 기준 환율
     ttb = item['ttb'] # 송금 받을때
     tts = item['tts'] # 송금 보낼때
-    bkpr = item['bkpr'] # 장부 가격
-    data_list.append([currency_name, currency_code, exchange_rate, ttb, tts, bkpr])
-    print(f"국가/통화명: {currency_name} ({currency_code}), 기준 환율: {exchange_rate}, 송금 받을때: {ttb}, 송금 보낼때: {tts}")
+    #bkpr = item['bkpr'] # 장부 가격
+    data_list.append([currency_name, currency_code, exchange_rate, ttb, tts])
+    # print(f"국가/통화명: {currency_name} ({currency_code}), 기준 환율: {exchange_rate}, 송금 받을때: {ttb}, 송금 보낼때: {tts}")
     # (연환가료율)"yy_efee_r":"0",(10일환가료율)"ten_dd_efee_r":"0",(서울외국환중개기준)"kftc_bkpr":"291","kftc_deal_bas_r":"291.7",
 
 window = Tk()
@@ -94,12 +94,29 @@ notebook.pack()
 frame1 = Frame(window)
 notebook.add(frame1, text='나라별 환율 정보')
 col_count = 1
+header = ['국가/통화명', '통화 코드', '기준 환율', '송금 받을때', '송금 보낼때']
 
-# 통화 정보 출력
+canvas = Canvas(frame1)
+scrollbar = Scrollbar(frame1, orient='vertical', command=canvas.yview)
+scrollable_frame = Frame(canvas)
+scrollable_frame.bind(
+    "<Configure>",
+    lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+)
+canvas.create_window((0, 0), window=scrollable_frame, anchor='nw')
+canvas.configure(yscrollcommand=scrollbar.set)
+
+scrollbar.pack(side='right', fill='y')
+canvas.pack(side='left', fill='both', expand=True)
+
+for i, row_name in enumerate(header):
+    label = tkinter.Label(scrollable_frame, text=row_name, font=("Helvetica", 14, "bold"), borderwidth=1, relief="raised", width=13, height=2)
+    label.grid(row=0, column=i+2)
+
 for i, currency_info in enumerate(data_list):
     for j, value in enumerate(currency_info):
-        label = tkinter.Label(frame1, text=str(value), font=("Helvetica", 12), borderwidth=3, relief="ridge")
-        label.grid(row=i, column=j+1)  # j+1을 사용하여 첫 번째 열은 헤더와 겹치지 않도록 설정
+        label = tkinter.Label(scrollable_frame, text=str(value), font=("Helvetica", 12), borderwidth=3, relief="ridge", width=17, height=2)
+        label.grid(row=i+1, column=j+2)  # Start from column 2 to avoid overlapping with the header
 col_count += 1
 
 # frame2 = Frame(window)
@@ -112,12 +129,20 @@ col_count += 1
 # gu_combo.pack()
 # Label(frame2, text='페이지2의 내용', fg='yellow', font='helvetica 48').pack()
 
+frame2 = Frame(window)
+notebook.add(frame2, text='기준 환율')
+Label(frame2, text='기준환율', fg='green', font='helvetica 48').pack()
+
 frame3 = Frame(window)
-notebook.add(frame3, text='페이지3')
-Label(frame3, text='페이지3의 내용', fg='green', font='helvetica 48').pack()
+notebook.add(frame3, text='송금 환율')
+Label(frame3, text='송금 받을때 : \n송금 보낼때 : ', fg='blue', font='helvetica 48').pack()
 
 frame4 = Frame(window)
-notebook.add(frame4, text='페이지4')
-Label(frame4, text='페이지4의 내용', fg='blue', font='helvetica 48').pack()
+notebook.add(frame4, text='나라 검색')
+Label(frame4, text='나라 검색', fg='green', font='helvetica 48').pack()
+
+frame5 = Frame(window)
+notebook.add(frame5, text='즐겨찾기')
+Label(frame5, text='즐겨찾기', fg='green', font='helvetica 48').pack()
 
 window.mainloop()
